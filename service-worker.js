ca0b6450c5ae4179e35c5c841226ec61a5376d5c
Habib İlgin <ucelhabib@gmail.com>
2026-06-23 15:05:50 +0300

    v96: Civata Seti kategori icine + ucelopen protokolu + UCEL_KlasorAc_Kurulum.reg

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strateji: HTML/JS için NETWORK-FIRST → her zaman taze sürüm, offline ise cache.
 // Görseller için CACHE-FIRST (statik).
 
-const CACHE_NAME = 'ucel-v95';
+const CACHE_NAME = 'ucel-v96';
 const STATIC_ASSETS = [
   './manifest.json',
   './icon-192.png',
