25a204a5454a230668b061d4117b81b47aff0d5d
Habib ILGIN <ucelhabib@gmail.com>
2026-07-22 15:59:10 +0300

    v173: Talep PDF kurumsal duzen - baslik orta, tarih sag ust, EBAT ayri kolon

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strateji: HTML/JS için NETWORK-FIRST → her zaman taze sürüm, offline ise cache.
 // Görseller için CACHE-FIRST (statik).
 
-const CACHE_NAME = 'ucel-v172';
+const CACHE_NAME = 'ucel-v173';
 const STATIC_ASSETS = [
   './manifest.json',
   './icon-192.png',
